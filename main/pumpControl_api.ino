71a4a53193827da248ce79e76c7b6bd8de2add9a
Sathishkumarsha <sathishkumar.ver2@gmail.com>
2025-09-06 07:56:08 +0530

    removed the main function outside the main folder - this file is a duplicate

--- main/pumpControl_api.ino
+++ main/pumpControl_api.ino
@@ -3,6 +3,16 @@
 // Generate the pump control in a function PumpControl_api()
 #define PUMP_PIN 12
 
+/**
+ * @brief Controls the water pump based on the soil moisture level.
+ *
+ * This function activates or deactivates the pump depending on the provided
+ * moisture level. If the moisture level is below 20, the pump is turned ON.
+ * If the moisture level is above 50, the pump is turned OFF.
+ * The function also prints the pump status ("PumpON" or "PumpOFF") to the Serial monitor.
+ *
+ * @param moistureLevel The current soil moisture level (integer value).
+ */
 void PumpControl_api(int moistureLevel) {
   if (moistureLevel < 20) {
     Serial.print("PumpON");
